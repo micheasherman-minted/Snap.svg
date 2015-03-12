399d5b1139e1c2a10f971a26519fc2b22ef125b4
Daniel Rogers <danielr@minted.com>
2015-03-12 11:20:13 -0700

    Change the order of amd/commonjs detection
    Snap defines both amd and commonjs, but it prefers amd.
    The internal dependency of eve does the same thing but prefers
    commonjs.  So loaders that make both systems break badly
    when loading two concatenated modules (like Webpack)

--- src/amd-banner.js
+++ src/amd-banner.js
@@ -1,14 +1,14 @@
 (function (glob, factory) {
     // AMD support
-    if (typeof define == "function" && define.amd) {
+    if (typeof exports != 'undefined') {
+        // Next for Node.js or CommonJS
+        var eve = require('eve');
+        module.exports = factory(glob, eve);
+    } else if (typeof define == "function" && define.amd) {
         // Define as an anonymous module
         define(["eve"], function (eve) {
             return factory(glob, eve);
         });
-    } else if (typeof exports != 'undefined') {
-        // Next for Node.js or CommonJS
-        var eve = require('eve');
-        module.exports = factory(glob, eve);
     } else {
         // Browser globals (glob is window)
         // Snap adds itself to window
